e98372ad3313fd8ecf8149e3b19bddf005ee972b
azerty197358 <138396654+azerty197358@users.noreply.github.com>
2025-12-21 19:13:34 +0100

    Update script.js

--- video-chat-omegle/script.js
+++ video-chat-omegle/script.js
@@ -19,7 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
   
   // ---------------------- قائمة فيديوهات الإعلانات ----------------------
   const adVideosList = [
-    'https://raw.githubusercontent.com/azerty197358/myads/main/Single%20girl%20video%20chat%20-%20Video%20Calls%20Apps%20(360p%2C%20h264).mp4',
     'https://raw.githubusercontent.com/azerty197358/myads/main/YouCut_20251221_081055765.mp4',
     'https://raw.githubusercontent.com/azerty197358/myads/main/YouCut_20251221_153328953.mp4';
   ];
